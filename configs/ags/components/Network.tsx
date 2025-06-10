1bdb2db3b931a4fbb6eac2445b0868f13f1de52d
tludwig <business@tludwig.dev>
2025-06-10 23:41:25 +0200

    fix: add check if wired is present

--- configs/ags/components/Network.tsx
+++ configs/ags/components/Network.tsx
@@ -1,17 +1,24 @@
 import { bind } from "astal";
 import AstalNetwork from "gi://AstalNetwork";
+import { GObject } from "/usr/share/astal/gjs";
+
+const DummyDevice = GObject.registerClass({
+    Properties: {
+        'iconName': GObject.ParamSpec.string('iconName', '', '', GObject.ParamFlags.READWRITE, 'network-wired-symbolic'),
+        'state': GObject.ParamSpec.int('state', '', '', GObject.ParamFlags.READWRITE, 0, 999, 100),
+    },
+}, class DummyDevice extends GObject.Object {});
 
 export default function Network() {
     const network = AstalNetwork.get_default();
 
-    const wifi = bind(network, "wifi");
-    const wifiIcon = bind(network.wifi, "iconName");
-    const dummyWired = {
-        iconName: "network-wired-symbolic",
-        state: 100, // NM.DeviceState.ACTIVATED
-    };
 
-    const wiredDevice = network.wired ?? dummyWired;
+    const wiredDevice = network.wired ?? new DummyDevice();
+    const wifiDevice = network.wifi ?? new DummyDevice();
+
+    const wifi = bind(network, "wifi");
+    const wifiIcon = bind(wifiDevice, "iconName");
+
     const wiredIcon = bind(wiredDevice, "iconName");
     const wiredState = bind(wiredDevice, "state");
 
