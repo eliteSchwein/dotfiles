cd5f65efde9fc1f9b8be21c80d1189e1f21b90d8
tludwig <business@tludwig.dev>
2025-07-21 20:00:27 +0200

    feat: add initial bluetooth connection

--- configs/ags/widgets/quicksettings/pages/BluetoothPage.tsx
+++ configs/ags/widgets/quicksettings/pages/BluetoothPage.tsx
@@ -1,9 +1,83 @@
-import AstalNetwork from "gi://AstalNetwork";
 import AstalBluetooth from "gi://AstalBluetooth";
 import { qsPage } from "../QSWindow";
 import { Gtk } from "astal/gtk4";
-import { bind } from "astal";
-import { bash } from "../../../utils";
+import {bind, GLib} from "astal";
+import {bash, notifySend} from "../../../utils";
+
+export function createBluetoothDevice(device: any) {
+  return {
+    label: device.get_name() || device.get_address(),
+    icon: (device.get_icon()) ? `${device.get_icon()}-symbolic` : null,
+    data: device,
+  }
+}
+
+export function connectBluetoothDevice(device: any) {
+    const bluetooth = AstalBluetooth.get_default();
+    const btAdapter = bluetooth.adapter;
+
+    if(btAdapter.get_discovering()) {
+        btAdapter.stop_discovery();
+    }
+    if(!btAdapter.get_discoverable()) {
+        btAdapter.set_discoverable(true);
+    }
+
+    device.data.connect_device(
+        (result: boolean, error: Error | null) => {
+            btAdapter.set_discoverable(false);
+            if (error) {
+                notifySend({
+                    urgency: "critical",
+                    appIcon: "bluetooth-active-symbolic",
+                    appName: "Bluetooth",
+                    summary: `Connection ${device.label} failed`,
+                    body: error.message,
+                });
+                console.error("Connection failed:", error.message);
+            } else {
+                console.log("Connected successfully:", result);
+            }
+        }
+    );
+}
+
+function BluetoothButton(device: any) {
+  device = device.device
+  return <centerbox cssClasses={device.data.get_connected() ? ["button", "bt-button", "active"] : ["button", "bt-button"]}>
+      <box halign={Gtk.Align.START}>
+        <image iconName={device.icon} />
+        <label label={device.label} />
+      </box>
+      <box></box>
+      <box halign={Gtk.Align.END}>
+        <Gtk.Separator />
+        {
+          device.data.get_paired()
+            ? <button
+                  iconName={device.data.get_connected() ? "bluetooth-active-symbolic" : "bluetooth-disconnected-symbolic"}
+                  halign={Gtk.Align.CENTER}
+                  valign={Gtk.Align.CENTER}
+                  onClicked={() => {
+                    console.log("clicked connect");
+                      connectBluetoothDevice(device);
+                  }}
+              >
+              </button>
+            : <button
+                  iconName="bluetooth-paired-symbolic"
+                  halign={Gtk.Align.CENTER}
+                  valign={Gtk.Align.CENTER}
+                  onClicked={() => {
+                    console.log("clicked pair");
+                    connectBluetoothDevice(device);
+                  }}
+              >
+              </button>
+        }
+      </box>
+    </centerbox>
+}
 
 export default function BluetoothPage() {
   const bluetooth = AstalBluetooth.get_default();
@@ -28,80 +102,67 @@ export default function BluetoothPage() {
           iconName={"go-previous-symbolic"}
         />
         <label label={"Bluetooth"} hexpand xalign={0} />
+
+        {bind(btAdapter, "discovering").as((state) => {
+          return (
+              <button
+                  onClicked={() => {
+                    state ? btAdapter.stop_discovery() : btAdapter.start_discovery()
+                  }}
+              >
+                <box>
+                  <image cssClasses={["mr-1"]} iconName={state ? "media-playback-stopped-symbolic" : "file-search-symbolic"} />
+                  <label label={state ? "Scanning" : "Scan"}/>
+                </box>
+              </button>)
+        })}
+
+          {bind(btAdapter, "discoverable").as((state) => {
+              return (
+                  <button
+                      onClicked={() => {
+                          btAdapter.set_discoverable(!state)
+                      }}
+                  >
+                      <box>
+                          <image cssClasses={["mr-1"]} iconName={state ? "feather-eye-symbolic" : "feather-eye-off-symbolic"} />
+                          <label label={state ? "Visible" : "Invisible"}/>
+                      </box>
+                  </button>)
+          })}
       </box>
       <Gtk.Separator />
       <Gtk.ScrolledWindow vexpand>
         <box vertical spacing={6}>
           {bind(bluetooth, "devices").as((devices) => {
             const pairedDevices = devices
-                .map((device) => ({
-                  label: device.get_name() || device.get_address(),
-                  data: device,
-                }))
+                .map((device) => (createBluetoothDevice(device)))
                 .filter((d) => d.data.get_paired());
 
             const discoveredDevices = devices
-                .map((device) => ({
-                  label: device.get_name() || device.get_address(),
-                  data: device,
-                }))
+                .map((device) => (createBluetoothDevice(device)))
                 .filter((d) => !d.data.get_paired());
 
             return (
                 <>
-                  <Gtk.Separator />
 
                   <Gtk.ScrolledWindow vexpand>
                     <box vertical spacing={6}>
-                      {pairedDevices.map((device, index) => (
-
-                          <button
-                              cssClasses={device.data.get_connected() ? ["button", "bt-button", "active"] : ["button", "bt-button"]}
-
-                          >
-                            <box>
-                              <image iconName={device.data.get_icon()} />
-                              <label label={device.label} />
-                              <Gtk.Separator />
-                              <box>
-                                <button
-                                    iconName={device.data.get_connected() ? "bluetooth-active-symbolic" : "bluetooth-disconnected-symbolic"}
-                                    halign={Gtk.Align.CENTER}
-                                    valign={Gtk.Align.CENTER}
-                                    onClicked={() => {
-                                      bash(`bluetoothctl connect ${device.data.get_address()}`);
-                                    }}
-                                >
-                                </button>
-                              </box>
-                            </box>
-                          </button>
-                      ))}
-                      <Gtk.Separator />
-                      {discoveredDevices.map((device, index) => (
-
-                          <button
-                              cssClasses={device.data.get_connected() ? ["button", "bt-button", "active"] : ["button", "bt-button"]}
-
-                          >
-                            <box>
-                              <image iconName={device.data.get_icon()} />
-                              <label label={device.label} />
-                              <Gtk.Separator />
-                              <box>
-                                <button
-                                    iconName={device.data.get_connected() ? "bluetooth-active-symbolic" : "bluetooth-disconnected-symbolic"}
-                                    halign={Gtk.Align.CENTER}
-                                    valign={Gtk.Align.CENTER}
-                                    onClicked={() => {
-                                      bash(`bluetoothctl connect ${device.data.get_address()}`);
-                                    }}
-                                >
-                                </button>
-                              </box>
-                            </box>
-                          </button>
-                      ))}
+                      {pairedDevices.length > 0 ? (
+                          <>
+                            {pairedDevices.map((device, index) => (
+                                <BluetoothButton key={index} device={device} />
+                            ))}
+                            <Gtk.Separator />
+                          </>
+                      ) : null}
+                      {discoveredDevices.length > 0 ? (
+                          <>
+                            {discoveredDevices.map((device, index) => (
+                                <BluetoothButton key={index} device={device} />
+                            ))}
+                          </>
+                      ) : null}
                     </box>
                   </Gtk.ScrolledWindow>
                 </>
