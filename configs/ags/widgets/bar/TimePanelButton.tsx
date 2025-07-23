61cec240573040dd229483e9e7235ae5b8d1e92a
tludwig <business@tludwig.dev>
2025-07-23 10:35:24 +0200

    feat: add desktop clock back feat: adjust border settings a bit

--- configs/ags/widgets/bar/TimePanelButton.tsx
+++ configs/ags/widgets/bar/TimePanelButton.tsx
@@ -1,15 +1,16 @@
-import { App } from "astal/gtk4";
+import { App, Astal, Gtk, Gdk } from "astal/gtk4";
 import { time } from "../../utils";
 import PanelButton from "../common/PanelButton";
 import { WINDOW_NAME } from "../datemenu/DateMenu";
+import { Binding } from "astal";
 
 export default function TimePanelButton({ format = "%H:%M:%S - %A %e." }) {
   return (
-    <PanelButton
-      window={WINDOW_NAME}
-      onClicked={() => App.toggle_window(WINDOW_NAME)}
-    >
-      <label label={time((t) => t.format(format)!)} />
-    </PanelButton>
+      <PanelButton
+          window={WINDOW_NAME}
+          onClicked={() => App.toggle_window(WINDOW_NAME)}
+      >
+        <label label={time((t) => t.format(format)!)} />
+      </PanelButton>
   );
 }
