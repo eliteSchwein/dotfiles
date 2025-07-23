c2a8529278199e6e5034bdcda7f7415767704d6f
tludwig <business@tludwig.dev>
2025-07-23 11:26:04 +0200

    fix: remove test functions

--- configs/ags/widgets/quicksettings/QSWindow.tsx
+++ configs/ags/widgets/quicksettings/QSWindow.tsx
@@ -69,7 +69,7 @@ function Header() {
             >
 
                 {bind(battery, "batteryIconName").as((icon) => {
-                    //if (icon === "battery-missing-symbolic") {
+                    if (icon === "battery-missing-symbolic") {
                         return (
                             <box spacing={2}>
                                 <image
@@ -90,7 +90,7 @@ function Header() {
                                 />
                             </box>
                         )
-                    //}
+                    }
 
                     return (
                         <box spacing={2}>
