9bbe51b9c59052dbc70889caa2f1d0b4c9ff91ff
tludwig <business@tludwig.dev>
2025-07-29 13:12:40 +0200

    feat: add 2 more fonts feaat: change fonts of whole ags

--- .config/ags/widgets/bar/TimePanelButton.tsx
+++ .config/ags/widgets/bar/TimePanelButton.tsx
@@ -2,7 +2,6 @@ import {App, Astal, Gtk, Gdk} from "astal/gtk4";
 import {time} from "../../utils";
 import PanelButton from "../common/PanelButton";
 import {WINDOW_NAME} from "../datemenu/DateMenu";
-import {Binding} from "astal";
 
 export default function TimePanelButton({format = "%H:%M:%S - %A %e."}) {
     return (
@@ -10,7 +9,7 @@ export default function TimePanelButton({format = "%H:%M:%S - %A %e."}) {
             window={WINDOW_NAME}
             onClicked={() => App.toggle_window(WINDOW_NAME)}
         >
-            <label label={time((t) => t.format(format)!)}/>
+            <label cssClasses={["font-digital7", "time-container"]} label={time((t) => t.format(format)!)}/>
         </PanelButton>
     );
 }
