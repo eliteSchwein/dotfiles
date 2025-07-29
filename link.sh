6a4cac4b788b0b986a458b83a74e2ab132a80c8f
tludwig <business@tludwig.dev>
2025-07-29 12:43:17 +0200

    chore: move ttf fonts into ttf subfolder fix: remove fonts from ignore filter fix: use correct victor mono font

--- link.sh
+++ link.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-stow --adopt -R .
+stow -v --adopt -R .
 
 cp $HOME/.local/share/TauonMusicBox/theme/Schw31n.ttheme $HOME/.local/share/TauonMusicBox/theme/Schw31nFIX.ttheme
+
+fc-cache
+
 echo "🎉 Dotfiles install complete!"
