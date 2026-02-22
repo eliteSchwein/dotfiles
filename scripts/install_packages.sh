4791e359ddfd693a532438641a83f0d66b4201ef
tludwig <business@tludwig.dev>
2026-02-22 19:08:16 +0100

    fix: add forgotten image viewer

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -19,7 +19,7 @@ paru -S \
 
 log_info "Install Addon Packages"
 paru -S \
-  ark dolphin-plugins kio-extras kio-fuse \
+  ark dolphin-plugins kio-extras kio-fuse gwenview \
   ydotool emote papirus-icon-theme-git satty \
   gimp clipse jq imagemagick \
   power-profiles-daemon hypridle \
