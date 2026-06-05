1b240d11c02f76b976ff791ef197478a614435ac
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2026-06-05 16:12:31 +0200

    Convert animation config to Lua format

--- .config/hypr/configs/animation.lua
+++ .config/hypr/configs/animation.lua
@@ -2,29 +2,48 @@
 
 hl.config({
     animations = {
-        enabled = "yes, please :)",
+        enabled = true,
     },
 })
 
-hl.bezier("easeOutQuint", 0.23, 1, 0.32, 1)
-hl.bezier("easeInOutCubic", 0.65, 0.05, 0.36, 1)
-hl.bezier("linear", 0, 0, 1, 1)
-hl.bezier("almostLinear", 0.5, 0.5, 0.75, 1.0)
-hl.bezier("quick", 0.15, 0, 0.1, 1)
+hl.curve("easeOutQuint", {
+    type = "bezier",
+    points = { {0.23, 1}, {0.32, 1} },
+})
 
-hl.animation([[global, 1, 10, default]])
-hl.animation([[border, 1, 5.39, easeOutQuint]])
-hl.animation([[windows, 1, 4.79, easeOutQuint]])
-hl.animation([[windowsIn, 1, 4.1, easeOutQuint, popin 87%]])
-hl.animation([[windowsOut, 1, 1.49, linear, popin 87%]])
-hl.animation([[fadeIn, 1, 1.73, almostLinear]])
-hl.animation([[fadeOut, 1, 1.46, almostLinear]])
-hl.animation([[fade, 1, 3.03, quick]])
-hl.animation([[layers, 1, 3.81, easeOutQuint]])
-hl.animation([[layersIn, 1, 4, easeOutQuint, fade]])
-hl.animation([[layersOut, 1, 1.5, linear, fade]])
-hl.animation([[fadeLayersIn, 1, 1.79, almostLinear]])
-hl.animation([[fadeLayersOut, 1, 1.39, almostLinear]])
-hl.animation([[workspaces, 1, 1.94, almostLinear, slidevert]])
-hl.animation([[workspacesIn, 1, 1.21, almostLinear, slidevert]])
-hl.animation([[workspacesOut, 1, 1.94, almostLinear, slidevert]])
+hl.curve("easeInOutCubic", {
+    type = "bezier",
+    points = { {0.65, 0.05}, {0.36, 1} },
+})
+
+hl.curve("linear", {
+    type = "bezier",
+    points = { {0, 0}, {1, 1} },
+})
+
+hl.curve("almostLinear", {
+    type = "bezier",
+    points = { {0.5, 0.5}, {0.75, 1.0} },
+})
+
+hl.curve("quick", {
+    type = "bezier",
+    points = { {0.15, 0}, {0.1, 1} },
+})
+
+hl.animation({ leaf = "global", enabled = true, speed = 10, curve = "default" })
+hl.animation({ leaf = "border", enabled = true, speed = 5.39, curve = "easeOutQuint" })
+hl.animation({ leaf = "windows", enabled = true, speed = 4.79, curve = "easeOutQuint" })
+hl.animation({ leaf = "windowsIn", enabled = true, speed = 4.1, curve = "easeOutQuint", style = "popin 87%" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 1.49, curve = "linear", style = "popin 87%" })
+hl.animation({ leaf = "fadeIn", enabled = true, speed = 1.73, curve = "almostLinear" })
+hl.animation({ leaf = "fadeOut", enabled = true, speed = 1.46, curve = "almostLinear" })
+hl.animation({ leaf = "fade", enabled = true, speed = 3.03, curve = "quick" })
+hl.animation({ leaf = "layers", enabled = true, speed = 3.81, curve = "easeOutQuint" })
+hl.animation({ leaf = "layersIn", enabled = true, speed = 4, curve = "easeOutQuint", style = "fade" })
+hl.animation({ leaf = "layersOut", enabled = true, speed = 1.5, curve = "linear", style = "fade" })
+hl.animation({ leaf = "fadeLayersIn", enabled = true, speed = 1.79, curve = "almostLinear" })
+hl.animation({ leaf = "fadeLayersOut", enabled = true, speed = 1.39, curve = "almostLinear" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = 1.94, curve = "almostLinear", style = "slidevert" })
+hl.animation({ leaf = "workspacesIn", enabled = true, speed = 1.21, curve = "almostLinear", style = "slidevert" })
+hl.animation({ leaf = "workspacesOut", enabled = true, speed = 1.94, curve = "almostLinear", style = "slidevert" })
