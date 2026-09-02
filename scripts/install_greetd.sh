72005e29a2e4189d6c326e2da7033d928f6280a7
Thomas Ludwig <business@tludwig.dev>
2026-09-03 01:24:08 +0200

    stuff

--- scripts/install_greetd.sh
+++ scripts/install_greetd.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
+
 source "$SCRIPT_DIR/logger.sh"
 
 log_info "Greetd Install: starting"
@@ -13,11 +15,16 @@ log_info "Install Greetd"
 paru -S greetd greetd-dms-greeter-git acl "${PACMAN_FLAGS[@]}"
 
 log_info "Copy Greetd Configs"
-sudo cp -af "$SCRIPT_DIR/no-stow-root/etc/greetd/config.toml" /etc/greetd/config.toml
-sudo cp -af "$SCRIPT_DIR/no-stow-root/etc/greetd/hypr.lua" /etc/greetd/hypr.lua
+sudo cp -af \
+    "$REPO_DIR/no-stow-root/etc/greetd/config.toml" \
+    /etc/greetd/config.toml
 
-USER_HOME="$HOME"
-sudo sed -i "s|\$HOME|$USER_HOME|g" /etc/greetd/hypr.lua
+sudo cp -af \
+    "$REPO_DIR/no-stow-root/etc/greetd/hypr.lua" \
+    /etc/greetd/hypr.lua
+
+log_info "Replace \$HOME in Greeter Hyprland Config"
+sudo sed -i "s|\$HOME|$HOME|g" /etc/greetd/hypr.lua
 
 log_info "Sync DankMaterialShell Greeter Configuration"
 DMS_PRIVESC=sudo dms-greeter sync
